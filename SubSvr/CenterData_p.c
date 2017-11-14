377fd3dcb63e66ab71db7f9ade41ee04ab01d9f4
heyuankuo <1421198728@qq.com>
2017-11-14 16:11:03 +0800

    【简要描述】 日常提交 【修 改 人】 何源阔

--- SubSvr/CenterData_p.c
+++ SubSvr/CenterData_p.c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Sun Nov 12 15:12:51 2017
+/* at Mon Nov 13 13:41:11 2017
  */
 /* Compiler settings for ..\comdata\CenterData.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
